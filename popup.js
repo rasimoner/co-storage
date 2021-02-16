549d1431b066abc3abec1f18a03f7484233119db
rasimoner <rasim-2550@hotmail.com>
2021-02-16 18:51:51 +0300

    Add files via upload

--- popup.js
+++ popup.js
@@ -17,18 +17,23 @@ changeColor.addEventListener("click", async () => {
 
 // The body of this function will be execuetd as a content script inside the
 // current page
-function setPageBackgroundColor() {
+async function setPageBackgroundColor() {
     chrome.storage.sync.get("color", ({color}) => {
         document.body.style.backgroundColor = color;
     });
 
-    // // Save it using the Chrome extension storage API.
-    // chrome.storage.sync.set({'foo': 'hello', 'bar': 'hi'}, function() {
-    //     console.log('Settings saved');
-    // });
-    //
-    // // Read it using the storage API
-    // chrome.storage.sync.get(['foo', 'bar'], function(items) {
-    //     message('Settings retrieved', items);
-    // });
+    // let asd = sessionStorage as [name: string]: any; 
+    let result = Object.keys(sessionStorage).map((key) => [String(key), sessionStorage[key]]);
+    let result2 = Object.entries(sessionStorage)
+    console.log("result2",result2);
+    
+    result2.forEach(element => {
+        console.log("element", element);
+    });
+    let test = 'ConfigurationService_getConfigurations__["SharedConfigurations"]';
+    let gettingItem = sessionStorage.getItem(test)
+
+    sessionStorage.setItem(test, '{"sharedConfigurations":{"additionalValueMustDiagnosticCodes":["07.02.1","20.01","21.01"],"allowExaminationDocumentSelectionOnEpicrisisCreate":false,"departmentAuthorityCode":"Yetki.Veri.Bolum","enableESignerDBLogging":true,"enableIncludeToEpicrisisUnsignedForms":false,"enablePrintButtonForForms":true,"featuredMonitoringFormCodes":["U07.3"],"floorAuthorityCode":"Yetki.Veri.Kat","generalPhysicalExaminationRequired":true,"isEnableAddingLabOrderDespiteOfRepeatedOnUss":false,"isEnableAddingRadOrderDespiteOfRepeatedOnTeletip":true,"key":"SharedConfigurations","prescriptionPermission":15,"pusulaWebAPIAnonymousUsername":"comed.doktor","reviewOfSystemGeneralRequired":false,"stockWarehouseAuthorityCode":"Yetki.Veri.Depo","temporaryTransferPropertyType":"HASTATRANSFER_Testssss"}}');
 }
+
+
