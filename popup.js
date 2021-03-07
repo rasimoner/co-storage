e3f08e953d0d9f99a5b1e880f5b7cf0775352056
rasimoner <rasim-2550@hotmail.com>
2021-03-07 15:08:11 +0300

    get session values

--- popup.js
+++ popup.js
@@ -1,11 +1,9 @@
-// Initialize butotn with users's prefered color
 let changeColor = document.getElementById("changeColor");
 
 chrome.storage.sync.get("color", ({color}) => {
     changeColor.style.backgroundColor = color;
 });
 
-// When the button is clicked, inject setPageBackgroundColor into current page
 changeColor.addEventListener("click", async () => {
     let [tab] = await chrome.tabs.query({active: true, currentWindow: true});
 
@@ -15,25 +13,58 @@ changeColor.addEventListener("click", async () => {
     });
 });
 
-// The body of this function will be execuetd as a content script inside the
-// current page
 async function setPageBackgroundColor() {
     chrome.storage.sync.get("color", ({color}) => {
         document.body.style.backgroundColor = color;
     });
 
-    // let asd = sessionStorage as [name: string]: any; 
-    let result = Object.keys(sessionStorage).map((key) => [String(key), sessionStorage[key]]);
-    let result2 = Object.entries(sessionStorage)
-    console.log("result2",result2);
-    
-    result2.forEach(element => {
-        console.log("element", element);
+    let sessionEntries = Object.entries(sessionStorage)
+    let groupName = document.getElementById('groupNames');
+
+    sessionEntries.forEach(entry => {
+        let parsed = JSON.parse(entry[1]);
+        let result = Object.keys(parsed).map((key) => [String(key), parsed[key]]);
+
+        let title = result?.[0]?.[0];
+        let values = result?.[0]?.[1];
+        console.log("title", title);
+        // groupName.options = title;
+        console.log("values", values);
+
     });
     let test = 'ConfigurationService_getConfigurations__["SharedConfigurations"]';
     let gettingItem = sessionStorage.getItem(test)
 
     sessionStorage.setItem(test, '{"sharedConfigurations":{"additionalValueMustDiagnosticCodes":["07.02.1","20.01","21.01"],"allowExaminationDocumentSelectionOnEpicrisisCreate":false,"departmentAuthorityCode":"Yetki.Veri.Bolum","enableESignerDBLogging":true,"enableIncludeToEpicrisisUnsignedForms":false,"enablePrintButtonForForms":true,"featuredMonitoringFormCodes":["U07.3"],"floorAuthorityCode":"Yetki.Veri.Kat","generalPhysicalExaminationRequired":true,"isEnableAddingLabOrderDespiteOfRepeatedOnUss":false,"isEnableAddingRadOrderDespiteOfRepeatedOnTeletip":true,"key":"SharedConfigurations","prescriptionPermission":15,"pusulaWebAPIAnonymousUsername":"comed.doktor","reviewOfSystemGeneralRequired":false,"stockWarehouseAuthorityCode":"Yetki.Veri.Depo","temporaryTransferPropertyType":"HASTATRANSFER_Testssss"}}');
+}
+
+function myFunction() {
+    let sessionEntries = Object.entries(sessionStorage);
+
+    var x = document.createElement("SELECT");
+    x.setAttribute("id", "mySelect");
+    document.body.appendChild(x);
+
+    sessionEntries.forEach(entry => {
+        let parsed = JSON.parse(entry[1]);
+        let result = Object.keys(parsed).map((key) => [String(key), parsed[key]]);
+
+        let title = result?.[0]?.[0];
+        let values = result?.[0]?.[1];
+
+        for (let item in values) {
+            var z = document.createElement("option");
+            z.setAttribute(values[item], values[item]);
+            var t = document.createTextNode(values[item]);
+            z.appendChild(t);
+            document.getElementById("mySelect").appendChild(z);
+        }
+
+        console.log("title", title);
+        // groupName?.options = title;
+        console.log("values", values);
+
+    });
 }
 
 
