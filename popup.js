79a67ddfaf63d5499aee1aca03ee51500c1fb261
rasimoner <rasim-2550@hotmail.com>
2021-03-12 20:32:11 +0300

    Update popup.js
    düzenle

--- popup.js
+++ popup.js
@@ -1,51 +1,71 @@
 let changeColor = document.getElementById("changeColor");
 
-chrome.storage.sync.get("color", ({color}) => {
+const extensionDocument = document;
+
+chrome.storage.sync.get("color", ({ color }) => {
     changeColor.style.backgroundColor = color;
 });
 
-changeColor.addEventListener("click", async () => {
-    let [tab] = await chrome.tabs.query({active: true, currentWindow: true});
 
-    chrome.scripting.executeScript({
-        target: {tabId: tab.id},
-        function: setPageBackgroundColor,
-    });
-});
-
-async function setPageBackgroundColor() {
-    chrome.storage.sync.get("color", ({color}) => {
+function setPageBackgroundColor(){
+    chrome.storage.sync.get("color", ({ color }) => {
         document.body.style.backgroundColor = color;
     });
 
-    let sessionEntries = Object.entries(sessionStorage)
-    let groupNames = document.getElementById("groupNames");
+    console.log('reads',sessionStorage)
+
+    return Object.entries(sessionStorage);
+
+    console.log({ document })
+    let groupNames = extensionDocument.getElementById("group-names");
+
+    let changeColor2 = extensionDocument.getElementById("changeColor");
+
+    console.log("groupNames", groupNames)
+    console.log("changeColor2", changeColor2)
 
     sessionEntries.forEach(entry => {
         let parsed = "";
-        if(entry[1].includes('{')) parsed = JSON.parse(entry[1]);
-        if(!parsed) return;
+        if (entry[1].includes('{')) parsed = JSON.parse(entry[1]);
+        if (!parsed) return;
 
         let result = Object.keys(parsed)?.map((key) => [String(key), parsed[key]]);
 
         let title = result?.[0]?.[0];
         let values = result?.[0]?.[1];
 
-        // groupNames[groupNames?.options?.length ?? 0] = new Option(title, values);
-        console.log("title", title);
-        // groupName.options = title;
-        console.log("values", values);
 
     });
-    
+
     let configGroupName = 'SharedConfigurations'; // group names lookuptan gelen grup başlık adı
     let configItemName = `ConfigurationService_getConfigurations__[${configGroupName}]`;
     let gettingItem = sessionStorage.getItem(configItemName);
 
-    console.log("gettingItem", gettingItem);
 
     sessionStorage.setItem(configItemName, '{"sharedConfigurations":{"additionalValueMustDiagnosticCodes":["07.02.1","20.01","21.01"],"allowExaminationDocumentSelectionOnEpicrisisCreate":false,"departmentAuthorityCode":"Yetki.Veri.Bolum","enableESignerDBLogging":true,"enableIncludeToEpicrisisUnsignedForms":false,"enablePrintButtonForForms":true,"featuredMonitoringFormCodes":["U07.3"],"floorAuthorityCode":"Yetki.Veri.Kat","generalPhysicalExaminationRequired":true,"isEnableAddingLabOrderDespiteOfRepeatedOnUss":false,"isEnableAddingRadOrderDespiteOfRepeatedOnTeletip":true,"key":"SharedConfigurations","prescriptionPermission":15,"pusulaWebAPIAnonymousUsername":"comed.doktor","reviewOfSystemGeneralRequired":false,"stockWarehouseAuthorityCode":"Yetki.Veri.Depo","temporaryTransferPropertyType":"HASTATRANSFER_Testssss"}}');
 }
+
+changeColor.addEventListener("click", async () => {
+    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+    chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: setPageBackgroundColor
+        
+    },(res) => {
+        const storage = res?.[0]?.result;
+
+        console.log('res',storage);
+
+        let groupNames = extensionDocument.getElementById("group-names");
+
+        let changeColor2 = extensionDocument.getElementById("changeColor");
+
+        console.log("groupNames", groupNames)
+        console.log("changeColor2", changeColor2)
+    });
+});
+
 
 function myFunction() {
     let sessionEntries = Object.entries(sessionStorage);
